d9e85c919866299682ce23644088b35c2f86551e
wumacms <wumacms@qq.com>
2025-07-18 14:00:44 +0800

    init

---
+++ Sources/SwiftUIButtonKit/PrimaryButton.swift
@@ -0,0 +1,26 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+import SwiftUI
+
+struct PrimaryButton: View {
+    private let title: String
+    private let action: () -> Void
+
+    public init(title: String, action: @escaping () -> Void) {
+        self.title = title
+        self.action = action
+    }
+
+    public var body: some View {
+        Button(action: action) {
+            Text(title)
+                .font(.headline)
+                .foregroundColor(.white)
+                .padding()
+                .frame(maxWidth: .infinity)
+                .background(Color.blue)
+                .cornerRadius(12)
+        }
+    }
+}
