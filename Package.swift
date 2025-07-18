0f4112192142bb2f56e1a4d83c44c12896ebf9e9
wumacms <wumacms@qq.com>
2025-07-18 17:21:38 +0800

    update

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "SwiftUIButtonKit",
     platforms: [
-        .iOS(.v12),
+        .iOS(.v13),
         .macOS(.v11),
         .tvOS(.v13),
         .watchOS(.v6)
